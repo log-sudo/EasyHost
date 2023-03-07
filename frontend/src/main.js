c25f1df81a14db9ea19dc9e7c3e7ac88d6a2362b
logXT <122947448+logXT@users.noreply.github.com>
2023-03-07 16:37:02 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,11 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import VueCodeMirror from 'vue-codemirror'
 
 import * as ElIconModules from '@element-plus/icons-vue'
 
 // 全局组件挂载
 const app = createApp(App)
-app.use(VueCodeMirror)
 app.use(ElementPlus)
 
 Object.keys(ElIconModules).forEach(function (key) {
